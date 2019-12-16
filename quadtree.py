9f4a99b8575c410b139adb11ce9926fcda35ebaa
cernicky.dav <cernicky.dav@gmail.com>
2019-12-16 09:31:44 +0100

    Funkce: load_file - dodělat load_xy - ok bbox - zkus načíst jen souřadnice

--- quadtree.py
+++ quadtree.py
@@ -1,1 +1,24 @@
-quadtree
+#   Funkce oddělí X a Y souřadnice od sebe a nejde jejich extrémy a vypočítá průměry které následně uloží
+def bbox(data):
+    coordinateX = []
+    for f in data['features']:
+        coordinateX.append(f['geometry']['coordinates'][0])
+
+    coordinateY = []
+    for f in data['features']:
+        coordinateY.append(f['geometry']['coordinates'][1])
+#   Maxima a minima
+    max_x = max(coordinateX)
+    min_x = min(coordinateX)
+    max_y = max(coordinateY)
+    min_y = min(coordinateY)
+#   Střed bounding boxu
+    mid_x = (max_x + min_x) / 2
+    mid_y = (max_y + min_y) / 2
+    print(mid_x,min_y)
+    return (mid_x, mid_y)
+
+def build_quadtree(file):
+    cluster_ID = 0
+    file_ID = {}
+    mid_x, mid_y = bbox(file)
--- split.py
+++ split.py
@@ -1,3 +1,4 @@
+from quadtree import bbox
 import json, os.path
 
 #   Funkce pro otoevření souboru .geojson
@@ -5,45 +6,28 @@ def load_file(json_file):
     if os.path.isfile(json_file):
         try:    # Zkus otevřít soubor
             with open(json_file, "r", encoding="utf-8") as f:
-                file = json.load(f)
+                data = json.load(f)
         except FileExistsError:
             print("Soubor " + json_file + " je poškozený, nebo se nejedná o korektní geojson.")
             exit()
     else:
         print("Soubor " + json_file + " neexistuje!")
         exit()
-    return file
+    return data
 
 #   Funkce pro načtení souřadnic z geojson
-def load_xy(file):
+def load_xy(data):
     coordinates = []    # seznam pro zapisování souřadnic
-    for f in file['features']:
+    for f in data['features']:
         xy = f['geometry']['coordinates']
         coordinates.append(xy)
     #print(xy)
     return coordinates
 
-#   Funkce oddělí X a Y souřadnice od sebe a nejde jejich extrémy, které následně uloží
-def bbox(coord):
-    coordinateX = []
-    for f in file['features']:
-        coordinateX.append(f['geometry']['coordinates'][0])
+data = load_file("export.geojson")
+#print(data)
 
-    coordinateY = []
-    for f in file['features']:
-        coordinateY.append(f['geometry']['coordinates'][1])
+coordinates = load_xy(data)
+print(coordinates)
 
-    max_x = max(coordinateX)
-    min_x = min(coordinateX)
-    max_y = max(coordinateY)
-    min_y = min(coordinateY)
-
-    return max_x,min_x,max_y,min_y
-
-file = load_file("export.geojson")
-#print(file)
-
-coordinates = load_xy(file)
-#print(coordinates)
-
-bbox(file)
+hm = bbox(data)
