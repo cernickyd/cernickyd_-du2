1e0f16274750e66585f7a2c5e2541d7bec6dcac4
cernicky.dav <cernicky.dav@gmail.com>
2019-12-18 16:22:43 +0100

    Funkční, ale!!

--- quadtree.py
+++ quadtree.py
@@ -1,24 +1,58 @@
-#   Funkce oddělí X a Y souřadnice od sebe a nejde jejich extrémy a vypočítá průměry které následně uloží
-def bbox(data):
+clusterID = 0
+xyID = {}
+
+def lines (xy, SZ, JV):
+    if len(xy) < 50:
+        global clusterID, xyID
+        for pt in xy:
+            xyID[(pt[0],pt[1])] = clusterID
+        clusterID += 1
+    else:
+        a1 = SZ
+        a2 = ((JV[0] + SZ[0]) / 2, SZ[1])
+        a3 = (SZ[0], (JV[1] + SZ[1]) / 2)
+        a4 = ((JV[0] + SZ[0]) / 2, (JV[1] + SZ[1]) / 2)
+        b1 = ((JV[0] + SZ[0]) / 2, (JV[1] + SZ[1]) / 2)
+        b2 = (JV[0], (JV[1] + SZ[1]) / 2)
+        b3 = ((JV[0] + SZ[0]) / 2, JV[1])
+        b4 = JV
+        q1 = []
+        q2 = []
+        q3 = []
+        q4 = []
+
+        for pt in xy:
+            if pt[0] >= a1[0] and pt[0] <= b1[0] and pt[1] >= b1[1] and pt[1] <= a1[1]:
+                q1.append(pt)
+            elif pt[0] >= a2[0] and pt[0] <= b2[0] and pt[1] >= b2[1] and pt[1] <= a2[1]:
+                q2.append(pt)
+            elif pt[0] >= a3[0] and pt[0] <= b3[0] and pt[1] >= b3[1] and pt[1] <= a3[1]:
+                q3.append(pt)
+            elif pt[0] >= a4[0] and pt[0] <= b4[0] and pt[1] >= b4[1] and pt[1] <= a4[1]:
+                q4.append(pt)
+        lines(q1, a1, b1)
+        lines(q2, a2, b2)
+        lines(q3, a3, b3)
+        lines(q4, b1, b4)
+
+def bbox(xy):
     coordinateX = []
-    for f in data['features']:
-        coordinateX.append(f['geometry']['coordinates'][0])
+    for f in xy:
+        coordinateX.append(f[0])
 
     coordinateY = []
-    for f in data['features']:
-        coordinateY.append(f['geometry']['coordinates'][1])
-#   Maxima a minima
-    max_x = max(coordinateX)
-    min_x = min(coordinateX)
-    max_y = max(coordinateY)
-    min_y = min(coordinateY)
-#   Střed bounding boxu
-    mid_x = (max_x + min_x) / 2
-    mid_y = (max_y + min_y) / 2
-    print(mid_x,min_y)
-    return (mid_x, mid_y)
+    for f in xy:
+        coordinateY.append(f[1])
 
-def build_quadtree(file):
-    cluster_ID = 0
-    file_ID = {}
-    mid_x, mid_y = bbox(file)
+    maxX = max(coordinateX)
+    minX = min(coordinateX)
+    maxY = max(coordinateY)
+    minY = min(coordinateY)
+    return (minX, maxY), (maxX, minY)
+
+def build_quadtree(xy):
+    SZ, JV = bbox(xy)
+    lines(xy, SZ, JV)
+    return xyID
+
+
--- split.py
+++ split.py
@@ -1,33 +1,42 @@
-from quadtree import bbox
+from quadtree import build_quadtree
 import json, os.path
 
-#   Funkce pro otoevření souboru .geojson
 def load_file(json_file):
     if os.path.isfile(json_file):
-        try:    # Zkus otevřít soubor
-            with open(json_file, "r", encoding="utf-8") as f:
-                data = json.load(f)
-        except FileExistsError:
-            print("Soubor " + json_file + " je poškozený, nebo se nejedná o korektní geojson.")
-            exit()
+        while True:
+            try:  # Zkus otevřít soubor
+                with open(json_file, "r", encoding="utf-8") as f:
+                    data = json.load(f)
+                    break
+            except FileExistsError as error:
+                print(error)
+                print("Soubor " + json_file + " je poškozený, nebo se nejedná o korektní geojson.")
+                exit()
     else:
         print("Soubor " + json_file + " neexistuje!")
         exit()
+
     return data
 
-#   Funkce pro načtení souřadnic z geojson
 def load_xy(data):
-    coordinates = []    # seznam pro zapisování souřadnic
-    for f in data['features']:
-        xy = f['geometry']['coordinates']
+    coordinates = []
+    for feature in data['features']:
+        xy = feature['geometry']['coordinates']
         coordinates.append(xy)
-    #print(xy)
+
     return coordinates
 
-data = load_file("export.geojson")
-#print(data)
+def add_ID(data, xyID):
+    for f in data['features']:
+        xy = f['geometry']['coordinates']
+        f['clusterID'] = xyID[(xy[0],xy[1])]
 
+    return data
+
+data = load_file("input.geojson")
 coordinates = load_xy(data)
-print(coordinates)
-
-hm = bbox(data)
+xyID = build_quadtree(coordinates)
+print(xyID)
+final_ID = add_ID(data,xyID)
+with open("output.geojson", "w", encoding="utf-8") as f:
+    json.dump(final_ID, f, indent=2)
